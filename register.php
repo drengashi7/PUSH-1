110047cbb0a78512c8041bd20c4fc7a9944764be
Milottty <milotymeri1@gmail.com>
2025-03-22 12:59:21 +0100

    login and register

---
+++ register.php
@@ -0,0 +1,46 @@
+<?php include_once 'header.php';?>
+
+ <div  class="w-50 mx-auto" style="padding-top: 100px;">
+    <h1 class="mb-3">Please sign up</h1>
+ <form>
+ <div data-mdb-input-init class="form-outline mb-4">
+    <input type="text" id="form2Example3" class="form-control" />
+    <label class="form-label" for="form2Example3">Name</label>
+  </div>
+  <div data-mdb-input-init class="form-outline mb-4">
+    <input type="text" id="form2Example4" class="form-control" />
+    <label class="form-label" for="form2Example4">Surname</label>
+  </div>
+  <div data-mdb-input-init class="form-outline mb-4">
+    <input type="text" id="form2Example6" class="form-control" />
+    <label class="form-label" for="form2Example6">Username</label>
+  </div>
+  <!-- Email input -->
+  <div data-mdb-input-init class="form-outline mb-4">
+    <input type="email" id="form2Example1" class="form-control" />
+    <label class="form-label" for="form2Example1">Email address</label>
+  </div>
+
+  <!-- Password input -->
+  <div data-mdb-input-init class="form-outline mb-4">
+    <input type="password" id="form2Example2" class="form-control" />
+    <label class="form-label" for="form2Example2">Password</label>
+  </div>
+  <!-- Submit button -->
+  <button  type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-block mb-4">Sign in</button>
+
+  <!-- Register buttons -->
+  <div class="text-center">
+    <p>Han an account? <a href="#!">Sign In</a></p>
+  </div>
+</form>
+ </div>
+
+
+
+    
+</body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+</html>
+
+<?php include_once 'footer.php';?>
